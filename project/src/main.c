a71ff002b8eb364be95b637f20ee08c453876acc
Aleksey Egorov <egorovaleksey0@mail.ru>
2020-10-21 12:32:51 +0200

    save

---
+++ project/src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+
+typedef struct {
+    u_int8_t value1 : 2;
+    u_int8_t value2 : 2;
+    u_int8_t value3 : 2;
+    u_int8_t value4 : 2;
+} Bit;
+
+int main() {
+
+}
